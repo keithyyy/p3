6152caa5df13217728ddc9e1befc49195521b06e
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-31 00:43:49 -0400

    adding 404 page

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Image from '../../images/404.png'
+
 function NoMatch() {
   return (
     <div>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
+      <h1 style={{marginTop: "30px", fontSize: "30px"}}>We got lost finding this for you but don't get lost finding yourself</h1>
+      <img style={{marginTop: "-50px"}} src={Image} ></img>
+
     </div>
   )
 }
